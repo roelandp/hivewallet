e7c1665aca1095275e53449145a873557afec246
Gregory Luneau <gregory.luneau@gmail.com>
2018-08-25 12:11:13 -0400

    eslint implementation and fixes

--- app/alloy.js
+++ app/alloy.js
@@ -10,79 +10,76 @@
 //
 // Alloy.Globals.someGlobalFunction = function(){};
 
-
 Alloy.Globals.measurement = require('/measurement');
 
 Alloy.Globals.isiPhoneX = (Ti.Platform.osname === 'iphone' && Ti.Platform.displayCaps.platformWidth === 375 && Ti.Platform.displayCaps.platformHeight === 812 && Ti.Platform.displayCaps.logicalDensityFactor === 3);
 
 // loading popover
-Alloy.Globals.loading = Alloy.createWidget("nl.fokkezb.loading");
+Alloy.Globals.loading = Alloy.createWidget('nl.fokkezb.loading');
 
 // filesys settings...
 var filesys = Titanium.Filesystem.applicationDataDirectory;
 
-//console.log('filesys path',filesys);
+// console.log('filesys path',filesys);
 
-if(OS_IOS) {
-  // disabling backups of filesystem on IOS
-  var fileDir = Titanium.Filesystem.getFile(filesys);
-  if(fileDir) {
-  fileDir.remoteBackup = false;
-  fileDir = null;
-  }
+if (OS_IOS) {
+	// disabling backups of filesystem on IOS
+	var fileDir = Titanium.Filesystem.getFile(filesys);
+	if (fileDir) {
+		fileDir.remoteBackup = false;
+		fileDir = null;
+	}
 }
-
 
 Alloy.Globals.filesys = filesys;
 
-var AvImageview = require("av.imageview");
+var AvImageview = require('av.imageview');
 Alloy.Globals.CONTENT_MODE_FIT = AvImageview.CONTENT_MODE_ASPECT_FIT;
 Alloy.Globals.CONTENT_MODE_FILL = AvImageview.CONTENT_MODE_ASPECT_FILL;
 
 var extrapad = 0;
 
-if(Alloy.Globals.isiPhoneX) {
-  extrapad = 70;
+if (Alloy.Globals.isiPhoneX) {
+	extrapad = 70;
 }
 
 var DP_platformWidth = (Ti.Platform.displayCaps.platformWidth);
 var DP_platformHeight = (Ti.Platform.displayCaps.platformHeight);
 
-if(OS_ANDROID) {
-  DP_platformWidth = Alloy.Globals.measurement.pxToDP(Ti.Platform.displayCaps.platformWidth);
-  DP_platformHeight = Alloy.Globals.measurement.pxToDP(Ti.Platform.displayCaps.platformHeight);
+if (OS_ANDROID) {
+	DP_platformWidth = Alloy.Globals.measurement.pxToDP(Ti.Platform.displayCaps.platformWidth);
+	DP_platformHeight = Alloy.Globals.measurement.pxToDP(Ti.Platform.displayCaps.platformHeight);
 }
 
 var widthratio = (DP_platformWidth / 414);
 var container_branding_top = 30 + extrapad;
 /* SIZES */
 Alloy.Globals.dimensions = {
-  container_branding_top: container_branding_top,
-  container_username_top: container_branding_top + 25,
-  container_welcome_top: (DP_platformHeight / 2) - (DP_platformHeight * 0.05),
-  DP_platformWidth: DP_platformWidth,
-  DP_platformHeight: DP_platformHeight,
-  overlay_container_height: DP_platformHeight - (container_branding_top + 100),
-  overlay_container_top: container_branding_top + 100,
-  qrsize: DP_platformWidth - 120,
-  avatar_top_right: -((300 / 375) * DP_platformWidth * 0.25),
-  avatar_width_height: (300 / 375) * DP_platformWidth,
-  overlay_barcode_top_height: (DP_platformHeight - (DP_platformWidth - 60)) /2,
-  overlay_barcode_middle_height: (DP_platformWidth - 60),
+	container_branding_top: container_branding_top,
+	container_username_top: container_branding_top + 25,
+	container_welcome_top: (DP_platformHeight / 2) - (DP_platformHeight * 0.05),
+	DP_platformWidth: DP_platformWidth,
+	DP_platformHeight: DP_platformHeight,
+	overlay_container_height: DP_platformHeight - (container_branding_top + 100),
+	overlay_container_top: container_branding_top + 100,
+	qrsize: DP_platformWidth - 120,
+	avatar_top_right: -((300 / 375) * DP_platformWidth * 0.25),
+	avatar_width_height: (300 / 375) * DP_platformWidth,
+	overlay_barcode_top_height: (DP_platformHeight - (DP_platformWidth - 60)) / 2,
+	overlay_barcode_middle_height: (DP_platformWidth - 60),
 };
 
-
 Alloy.Globals.colors = {
-  steemlightblue: "#4BA2F2",
-  steemdarkblue: "#1A5099",
-  themeblue: "#9013FE"
+	steemlightblue: '#4BA2F2',
+	steemdarkblue: '#1A5099',
+	themeblue: '#9013FE'
 };
 
 Alloy.Globals.config = {
-  apiurl : 'https://api.steemit.com',
+	apiurl: 'https://api.steemit.com',
 	expiretime: 30 * 1000, // default transaction expiretime
-  cmc_steem: "https://api.coinmarketcap.com/v2/ticker/1230/?convert=BTC",
-  cmc_sbd: "https://api.coinmarketcap.com/v2/ticker/1312/?convert=BTC",
-  teamid: "M2FNGBQ5GU",
-  walletfilename: "wallet.json",
-}
+	cmc_steem: 'https://api.coinmarketcap.com/v2/ticker/1230/?convert=BTC',
+	cmc_sbd: 'https://api.coinmarketcap.com/v2/ticker/1312/?convert=BTC',
+	teamid: 'M2FNGBQ5GU',
+	walletfilename: 'wallet.json',
+};
--- app/lib/functions.js
+++ app/lib/functions.js
@@ -10,33 +10,33 @@ function functions() {
 
 	var apicounter = 1;
 	this.prepareAPIbody = function (method, params) {
-		//function to fill the api body
+		// function to fill the api body
 		apicounter++;
 
-		var baseobj = {"jsonrpc":"2.0", "method":"condenser_api."+method, "id":apicounter};
+		var baseobj = { jsonrpc: '2.0', method: 'condenser_api.' + method, id: apicounter };
 
-		if(params) {
+		if (params) {
 			baseobj['params'] = params;
 		}
 
 		return baseobj;
 	};
 
-	this.formatToLocale = function(number, digits) {
+	this.formatToLocale = function (number, digits) {
 		return parseFloat(number).toLocaleString(Titanium.Locale.getCurrentLocale(), { minimumFractionDigits: digits, maximumFractionDigits: digits });
 	};
 
-	this.getUserObject = function(username) {
+	this.getUserObject = function (username) {
 		var currentaccounts = Ti.App.Properties.getObject('accounts');
 
-		if(currentaccounts.length == 0) {
+		if (currentaccounts.length == 0) {
 			return false;
 		}
 
-		for(var i =0; i < currentaccounts.length; i++) {
+		for (var i = 0; i < currentaccounts.length; i++) {
 			// loop through currentaccounts, break if found.
-			if(currentaccounts[i]['name'] == username) {
-				//found account to delete, remove it now.
+			if (currentaccounts[i]['name'] == username) {
+				// found account to delete, remove it now.
 
 				return currentaccounts[i];
 
@@ -45,74 +45,74 @@ function functions() {
 		}
 	};
 
-	this.formatUserBalanceObject = function(uo) {
+	this.formatUserBalanceObject = function (uo) {
 		return {
-			'balance': uo.balance,
-			'sbd_balance': uo.sbd_balance,
-			'savings_balance': uo.savings_balance,
-			'savings_sbd_balance': uo.savings_sbd_balance,
-			'name': uo.name,
-			'last_updated': Date.now(),
-			'privatekey': uo.privatekey,
-			'image': uo.image,
+			balance: uo.balance,
+			sbd_balance: uo.sbd_balance,
+			savings_balance: uo.savings_balance,
+			savings_sbd_balance: uo.savings_sbd_balance,
+			name: uo.name,
+			last_updated: Date.now(),
+			privatekey: uo.privatekey,
+			image: uo.image,
 		};
 	};
 
-	this.updateUserObject = function(account,key,value) {
+	this.updateUserObject = function (account, key, value) {
 
 		var currentaccounts = Ti.App.Properties.getObject('accounts');
 
 		var accountstosave = [];
 
-			for(var j =0; j < currentaccounts.length; j++) {
+		for (var j = 0; j < currentaccounts.length; j++) {
 
-				if(currentaccounts[j].name == account) {
-					// account found update value where key = key.
-					currentaccounts[j][key] = value;
-				}
-
-				accountstosave.push(currentaccounts[j]);
+			if (currentaccounts[j].name == account) {
+				// account found update value where key = key.
+				currentaccounts[j][key] = value;
 			}
 
+			accountstosave.push(currentaccounts[j]);
+		}
+
 		Ti.App.Properties.setObject('accounts', accountstosave);
-		//console.log('accounts object',Ti.App.Properties.getObject('accounts'));
+		// console.log('accounts object',Ti.App.Properties.getObject('accounts'));
 	};
 
-	this.steemAPIcall = function(method,params,cbres,cberr) {
+	this.steemAPIcall = function (method, params, cbres, cberr) {
 		this.xhrcall(
 			Alloy.Globals.config.apiurl,
 			'POST',
 			false,
-			function(e){
+			function (e) {
 				// simple validation here.
 
 				try {
 					// try parse the response...
-					//console.log(e);
+					// console.log(e);
 					var result = JSON.parse(e);
-					if("error" in result) {
+					if ('error' in result) {
 						cberr(result.error);
 					} else {
 						cbres(result);
 					}
 
-				} catch(e) {
+				} catch (e) {
 					cberr(e.message);
 				}
 			},
 			cberr,
-				this.prepareAPIbody(method,params)
-			);
+			this.prepareAPIbody(method, params)
+		);
 	};
 
 	this.xhrcall = function (urli, transport, fn_progress, fn_complete, fn_error, vars) {
 		var file_obj;
 
-		//Ti.API.info('####### XHR CALL REQUESTED TO ::::: ' + urli);
+		// Ti.API.info('####### XHR CALL REQUESTED TO ::::: ' + urli);
 
 		if (vars) {
 
-			//Ti.API.info(vars);
+			// Ti.API.info(vars);
 		}
 
 		try {
@@ -122,13 +122,13 @@ function functions() {
 
 				c.ondatastream = function (e) {
 					if (fn_progress) {
-						fn_progress({ 'progress': e.progress, 'kb': 0 });
+						fn_progress({ progress: e.progress, kb: 0 });
 					}
 				};
 
 				c.onsendstream = function (e) {
 					if (fn_progress) {
-						fn_progress({ 'progress': e.progress, 'kb': 0 });
+						fn_progress({ progress: e.progress, kb: 0 });
 					}
 				};
 
@@ -150,7 +150,7 @@ function functions() {
 
 				c.open(transport, urli);
 
-				c.setRequestHeader("Content-Type", "application/json");
+				c.setRequestHeader('Content-Type', 'application/json');
 				c.setRequestHeader('charset', 'utf-8');
 				if (vars && transport == 'POST') {
 					c.send(JSON.stringify(vars));
