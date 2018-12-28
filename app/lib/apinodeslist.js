f4370b2a3eb3da447edc81fbafd60f5aa69eee56
roelandp <248064+roelandp@users.noreply.github.com>
2018-12-28 15:11:39 +0100

    updated apinode list with default (now active) nodes

--- app/lib/apinodeslist.js
+++ app/lib/apinodeslist.js
@@ -4,27 +4,19 @@ module.exports = exports = [
     url: 'https://api.steemit.com'
   },
   {
+    maintainer: '@minnowsupport',
+    url: 'https://steemd.minnowsupportproject.org'
+  },
+  {
     maintainer: '@gtg',
     url: 'https://api.steem.house'
-  },
-  {
-    maintainer: '@privex',
-    url: 'https://steemd.privex.io'
-  },
-  {
-    maintainer: '@buildteam',
-    url: 'https://rpc.buildteam.io'
   },
   {
     maintainer: '@ausbitbank',
     url: 'https://rpc.steemviz.com'
   },
   {
-    maintainer: '@minnowsupport',
-    url: 'https://steemd.minnowsupportproject.org'
+    maintainer: '@privex',
+    url: 'https://steemd.privex.io'
   },
-  {
-  maintainer: '@netuoso',
-  url: 'https://rpc.steemliberator.com'
-  }
 ];
